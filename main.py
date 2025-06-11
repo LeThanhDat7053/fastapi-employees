453db941ae0c1216f57b85dddfdcf241d8bae622
mysite <datthanh705370@gmail.com>
2025-06-11 16:14:25 +0700

    Fix lan 8

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ def create_employee(employee: EmployeeCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_employee)
     return new_employee
+
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Railway dùng biến PORT
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
