42d80ac7db5ddbf44ba7ffbdbff15ef5b62b2077
mysite <datthanh705370@gmail.com>
2025-06-11 15:50:12 +0700

    Fix lan 6

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ def create_employee(name: str, position: str, db: Session = Depends(get_db)):
 def read_employees(db: Session = Depends(get_db)):
     return db.query(models.Employee).all()
 
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
