2310e59663002cbd2a665b46d54c8fea2096fef4
mysite <datthanh705370@gmail.com>
2025-06-11 15:05:01 +0700

    Fix lan 2

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/fast_api"
+DATABASE_URL = "mysql+pymysql://root:MznpvtckmscIDpexrZGgmEgXbLbybqKL@mysql.railway.internal:3306/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
